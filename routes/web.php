f4a620d74274f28909157c840d335f1722ac1d82
sohan <sohan01772@gmail.com>
2023-11-18 17:16:41 +0600

    module8

---
+++ routes/web.php
@@ -0,0 +1,12 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ProfileController;
+
+
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/user', [ProfileController::class,'index']);
